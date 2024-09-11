3f165334b9c221a2f54af746b15b8d3a59665bda
forna <fornariyanis99@gmail.com>
2024-09-11 14:08:39 +0200

    Add project files.

---
+++ FantasyMonsterFight.cpp
@@ -0,0 +1,10 @@
+// FantasyMonsterFight.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+
+int main()
+{
+    std::cout << "Bien le bonjour invocateur,  ";
+}
+
